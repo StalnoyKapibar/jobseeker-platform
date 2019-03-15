04e759bff9128603add46e29b2ea2513880e1804
Nikolay <sopilov@list.ru>
2019-03-15 23:34:41 +0300

    create project maven, add string boot

---
+++ src/main/java/com/jm/jobseekerplatform/JobseekerPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.jm.jobseekerplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobseekerPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobseekerPlatformApplication.class, args);
+    }
+
+}
