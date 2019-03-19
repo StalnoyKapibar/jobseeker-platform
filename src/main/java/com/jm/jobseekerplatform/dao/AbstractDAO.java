a04fcb815b8669bbe7347f840d0b16894c506cfb
sopilov <sopilov@list.ru>
2019-03-19 23:58:52 +0300

    add logic to AbstractDAO, AbstractService

--- src/main/java/com/jm/jobseekerplatform/dao/AbstractDAO.java
+++ src/main/java/com/jm/jobseekerplatform/dao/AbstractDAO.java
@@ -1,4 +1,38 @@
 package com.jm.jobseekerplatform.dao;
 
-public class AbstractDAO {
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository("abstractDAO")
+public abstract class AbstractDAO {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+
+    public void add(Object o) {
+        entityManager.persist(o);
+    }
+
+    public void update(Object o) {
+        entityManager.merge(o);
+
+    }
+
+
+    public abstract List getAll();
+
+
+    public Object getById(Long id) {
+        return entityManager.find(Object.class, id);
+    }
+
+
+    public void deleteById(Long id) {
+        Object obj = getById(id);
+        entityManager.remove(obj);
+
+    }
 }
--- src/main/java/com/jm/jobseekerplatform/service/AbstractService.java
+++ src/main/java/com/jm/jobseekerplatform/service/AbstractService.java
@@ -1,4 +1,36 @@
 package com.jm.jobseekerplatform.service;
 
-public class AbstractService {
+import com.jm.jobseekerplatform.dao.AbstractDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public abstract class AbstractService {
+
+    @Autowired
+    private AbstractDAO abstractDAO;
+
+    public void add(Object o) {
+        abstractDAO.add(o);
+    }
+
+
+    public void update(Object o) {
+        abstractDAO.update(o);
+    }
+
+
+    public List getAll() {
+        return abstractDAO.getAll();
+    }
+
+
+    public Object getById(Long id) {
+        return abstractDAO.getById(id);
+    }
+
+
+    public void deleteById(Long id) {
+        abstractDAO.deleteById(id);
+    }
 }
