bc367ec4001f2a448bc975a27cf39a7cd44d4a37
Nikolay Sopilov <43342760+sopilov@users.noreply.github.com>
2019-03-26 17:26:05 +0300

    add models DAO

---
+++ src/main/java/com/jm/jobseekerplatform/dao/impl/OfferDAO.java
@@ -0,0 +1,8 @@
+package com.jm.jobseekerplatform.dao.impl;
+
+import com.jm.jobseekerplatform.dao.AbstractDAO;
+import com.jm.jobseekerplatform.model.Offer;
+
+public class OfferDAO extends AbstractDAO<Offer> {
+
+}
---
+++ src/main/java/com/jm/jobseekerplatform/dao/impl/ResumeDAO.java
@@ -0,0 +1,8 @@
+package com.jm.jobseekerplatform.dao.impl;
+
+import com.jm.jobseekerplatform.dao.AbstractDAO;
+import com.jm.jobseekerplatform.model.Resume;
+
+public class ResumeDAO extends AbstractDAO<Resume> {
+
+}
