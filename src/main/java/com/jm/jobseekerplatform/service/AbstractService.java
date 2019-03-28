00296d3861b5c593fa81029af26d3f4125a47be7
Nikolay Sopilov <43342760+sopilov@users.noreply.github.com>
2019-03-28 14:57:09 +0300

    add init userroles and users to base

--- src/main/java/com/jm/jobseekerplatform/service/AbstractService.java
+++ src/main/java/com/jm/jobseekerplatform/service/AbstractService.java
@@ -2,10 +2,12 @@ package com.jm.jobseekerplatform.service;
 
 import com.jm.jobseekerplatform.dao.AbstractDAO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.io.Serializable;
 import java.util.List;
 
+@Transactional
 public abstract class AbstractService <T extends Serializable> {
 
     @Autowired
