5bec9cf7b125d1cf2e5aaa3c2c4969a7d60c904e
Nikolay Sopilov <43342760+sopilov@users.noreply.github.com>
2019-03-28 13:54:50 +0300

    add method encodePassword to UserService

--- src/main/java/com/jm/jobseekerplatform/service/impl/UserService.java
+++ src/main/java/com/jm/jobseekerplatform/service/impl/UserService.java
@@ -5,6 +5,7 @@ import com.jm.jobseekerplatform.model.User;
 import com.jm.jobseekerplatform.service.AbstractService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,7 +17,14 @@ public class UserService extends AbstractService<User> {
     @Qualifier("userDAO")
     private UserDAO dao;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public User findByLogin(String login) {
         return dao.findByLogin(login);
+    }
+
+    public String encodePassword(String password) {
+        return passwordEncoder.encode(password);
     }
 }
