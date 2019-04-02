18dfb8b68818a92f2f7b5dcdbdc9c703b37e7eeb
Nikolay Sopilov <43342760+sopilov@users.noreply.github.com>
2019-04-02 18:15:29 +0300

    add Vacancy's model, DAO, Service

---
+++ src/main/java/com/jm/jobseekerplatform/service/impl/VacancyService.java
@@ -0,0 +1,18 @@
+package com.jm.jobseekerplatform.service.impl;
+
+import com.jm.jobseekerplatform.dao.impl.VacancyDAO;
+import com.jm.jobseekerplatform.model.Vacancy;
+import com.jm.jobseekerplatform.service.AbstractService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service("vacancyService")
+@Transactional
+public class VacancyService extends AbstractService<Vacancy> {
+
+    @Autowired
+    private VacancyDAO dao;
+
+
+}
