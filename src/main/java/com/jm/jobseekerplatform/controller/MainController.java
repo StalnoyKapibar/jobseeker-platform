9b6139f1eeb55c95c5739aa3980e382babddcdb0
Nikolay Sopilov <43342760+sopilov@users.noreply.github.com>
2019-04-03 17:37:57 +0300

    add employer view

--- src/main/java/com/jm/jobseekerplatform/controller/MainController.java
+++ src/main/java/com/jm/jobseekerplatform/controller/MainController.java
@@ -1,14 +1,28 @@
 package com.jm.jobseekerplatform.controller;
 
+import com.jm.jobseekerplatform.model.EmployerProfile;
+import com.jm.jobseekerplatform.service.impl.EmployerProfileService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
+
+    @Autowired
+    private EmployerProfileService employerProfileService;
 
     @RequestMapping("/")
     public String mainPage() {
         return "index";
     }
 
+    @RequestMapping("/employer/{employerProfileId}")
+    public String employerProfilePage(@PathVariable Long employerProfileId, Model model) {
+        EmployerProfile employerProfile = employerProfileService.getById(employerProfileId);
+        model.addAttribute("eprofile", employerProfile);
+        return "employer";
+    }
 }
