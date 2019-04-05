cca0bde89c34ed9ebf1adbbb8dfc48929072b999
Nikolay Sopilov <43342760+sopilov@users.noreply.github.com>
2019-04-05 16:45:17 +0300

    add view salary in vacancies

--- src/main/resources/static/js/employer.js
+++ src/main/resources/static/js/employer.js
@@ -8,12 +8,24 @@ function showVacancy(id) {
             $("#VMCity").text(data.city);
             $("#VMDescription").text(data.description);
 
+            var str = "Зарплата: ";
+
+            if (data.salaryMin!=null ){
+                str = str + "от " + data.salaryMin + " рублей ";
+            }
+            if (data.salaryMax!=null ){
+                str = str + "до " + data.salaryMax + " рублей";
+            }
+            if (str == "Зарплата: "){
+                str = "Зарплата не указана";
+            }
+            $("#VMSalary").text(str);
+
             if (data.remote) {
                 $('#VMRemote').show();
             }else{
                 $('#VMRemote').hide();
             }
-
 
         }
     });
