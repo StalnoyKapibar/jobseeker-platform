052aca64ef5a8274c4b335f9746a2a97ed4755e0
sopilov <sopilov@list.ru>
2019-04-11 00:57:08 +0300

    new SeekerProfile

--- src/main/java/com/jm/jobseekerplatform/model/SeekerProfile.java
+++ src/main/java/com/jm/jobseekerplatform/model/SeekerProfile.java
@@ -1,7 +1,9 @@
 package com.jm.jobseekerplatform.model;
 
+import org.hibernate.annotations.Type;
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Set;
 
 @Entity
 @Table(name = "seekerprofiles")
@@ -11,13 +13,78 @@ public class SeekerProfile implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "name", nullable = false)
+    @Column(name = "name")
     private String name;
 
-    @Column(name = "middlename", nullable = false)
-    private String middleName;
+    @Column(name = "stack")
+    private String stack;
 
-    @Column(name = "surname", nullable = false)
-    private String surname;
+    @Column(name = "description", columnDefinition = "mediumtext")
+    private String description;
 
+    @Column(name = "photo")
+    @Type(type = "image")
+    private byte[] photo;
+
+    @OneToMany(fetch = FetchType.EAGER)
+    private Set<Portfolio> portfolios;
+
+    public SeekerProfile() {
+    }
+
+    public SeekerProfile(String name, String stack, String description, byte[] photo, Set<Portfolio> portfolios) {
+        this.name = name;
+        this.stack = stack;
+        this.description = description;
+        this.photo = photo;
+        this.portfolios = portfolios;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStack() {
+        return stack;
+    }
+
+    public void setStack(String stack) {
+        this.stack = stack;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public byte[] getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(byte[] photo) {
+        this.photo = photo;
+    }
+
+    public Set<Portfolio> getPortfolios() {
+        return portfolios;
+    }
+
+    public void setPortfolios(Set<Portfolio> portfolios) {
+        this.portfolios = portfolios;
+    }
 }
