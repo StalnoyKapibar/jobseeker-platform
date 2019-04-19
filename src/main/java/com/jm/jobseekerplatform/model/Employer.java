100aff24f59df0780ac33656cde0ead0e516dbbc
Nikolay Sopilov <43342760+sopilov@users.noreply.github.com>
2019-04-19 18:31:24 +0300

    add Employer

---
+++ src/main/java/com/jm/jobseekerplatform/model/Employer.java
@@ -0,0 +1,30 @@
+package com.jm.jobseekerplatform.model;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employers")
+public class Employer extends User {
+
+    @OneToOne(fetch = FetchType.EAGER)
+    private EmployerProfile employerProfile;
+
+    public Employer() {
+    }
+
+    public Employer(String login, char[] password, String email, UserRole authority, EmployerProfile employerProfile) {
+        super(login, password, email, authority);
+        this.employerProfile = employerProfile;
+    }
+
+    public EmployerProfile getEmployerProfile() {
+        return employerProfile;
+    }
+
+    public void setEmployerProfile(EmployerProfile employerProfile) {
+        this.employerProfile = employerProfile;
+    }
+}
---
+++ src/main/java/com/jm/jobseekerplatform/service/impl/EmployerService.java
@@ -0,0 +1,16 @@
+package com.jm.jobseekerplatform.service.impl;
+
+import com.jm.jobseekerplatform.dao.impl.EmployerDAO;
+import com.jm.jobseekerplatform.model.Employer;
+import com.jm.jobseekerplatform.service.AbstractService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service("employerService")
+@Transactional
+public class EmployerService extends AbstractService<Employer> {
+
+    @Autowired
+    private EmployerDAO dao;
+}
