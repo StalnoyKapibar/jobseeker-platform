486ee3a6b084cf1b706527a37d38daa577f76e8c
Nikolay Sopilov <43342760+sopilov@users.noreply.github.com>
2019-04-23 18:04:25 +0300

    add getByTags method to Vacancies DAO, Service

--- src/main/java/com/jm/jobseekerplatform/dao/impl/VacancyDAO.java
+++ src/main/java/com/jm/jobseekerplatform/dao/impl/VacancyDAO.java
@@ -1,10 +1,25 @@
 package com.jm.jobseekerplatform.dao.impl;
 
 import com.jm.jobseekerplatform.dao.AbstractDAO;
+import com.jm.jobseekerplatform.model.Tag;
 import com.jm.jobseekerplatform.model.Vacancy;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Repository("vacancyDAO")
 public class VacancyDAO extends AbstractDAO<Vacancy> {
 
+    public Set<Vacancy> getByTags(Set<Tag> tags) {
+        Set<Vacancy> vacancies = new HashSet<>();
+        for (Tag tag : tags) {
+            vacancies.addAll(entityManager
+                    .createQuery("SELECT v FROM Vacancy v JOIN v.tags t WHERE t.id = :param", Vacancy.class)
+                    .setParameter("param", tag.getId()).getResultList());
+        }
+        return vacancies;
+    }
 }
