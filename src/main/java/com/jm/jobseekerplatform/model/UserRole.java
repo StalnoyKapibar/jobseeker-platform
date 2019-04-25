dfc7a00f3e3cde8f461656dfafe3b9c04f30f8e1
Nikolay Sopilov <43342760+sopilov@users.noreply.github.com>
2019-04-25 15:44:30 +0300

    add equals in UserRole

--- src/main/java/com/jm/jobseekerplatform/model/UserRole.java
+++ src/main/java/com/jm/jobseekerplatform/model/UserRole.java
@@ -4,6 +4,7 @@ import org.springframework.security.core.GrantedAuthority;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Objects;
 
 @Entity
 @Table(name = "userroles")
@@ -37,5 +38,18 @@ public class UserRole implements Serializable, GrantedAuthority {
 
     public void setAuthority(String authority) {
         this.authority = authority;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserRole userRole = (UserRole) o;
+        return Objects.equals(authority, userRole.authority);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, authority);
     }
 }
