b40f49b1bf04cbebfa0935703b37e9156cf71ee8
sopilov <sopilov@list.ru>
2019-05-12 01:31:40 +0300

    add VerificationToken's class, DAO, Service

---
+++ src/main/java/com/jm/jobseekerplatform/service/impl/VerificationTokenService.java
@@ -0,0 +1,55 @@
+package com.jm.jobseekerplatform.service.impl;
+
+import com.jm.jobseekerplatform.dao.impl.VerificationTokenDAO;
+import com.jm.jobseekerplatform.model.User;
+import com.jm.jobseekerplatform.model.VerificationToken;
+import com.jm.jobseekerplatform.service.AbstractService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Date;
+
+@Service("verificationTokenService")
+@Transactional
+public class VerificationTokenService extends AbstractService<VerificationToken> {
+
+    @Autowired
+    private VerificationTokenDAO dao;
+
+    @Autowired
+    private UserService userService;
+
+    private Date calculateExpiryDate() {
+        int expiryTimeInMinutes = 60 * 24;
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Timestamp(calendar.getTime().getTime()));
+        calendar.add(Calendar.MINUTE, expiryTimeInMinutes);
+        return new Date(calendar.getTime().getTime());
+    }
+
+    public void createVerificationToken(String token, User user) {
+        VerificationToken verificationToken = new VerificationToken(token, user, calculateExpiryDate());
+        dao.add(verificationToken);
+    }
+
+    public VerificationToken findByToken(String token) {
+        return dao.findByToken(token);
+    }
+
+    public boolean tokenIsNonExpired(VerificationToken token) {
+        Calendar calendar = Calendar.getInstance();
+        boolean isNonExpired = (token.getExpiryDate().getTime() - calendar.getTime().getTime()) >= 0;
+        if (!isNonExpired) {
+            dao.delete(token);
+        }
+        return isNonExpired;
+    }
+
+    public void completeRegistration(VerificationToken token) {
+        token.getUser().setEnabled(true);
+        dao.delete(token);
+    }
+}
