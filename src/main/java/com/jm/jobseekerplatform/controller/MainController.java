7de133be80c006a775b0c792e46434035854fc15
Nikolay Sopilov <43342760+sopilov@users.noreply.github.com>
2019-05-16 22:54:30 +0300

    fix after merge

--- src/main/java/com/jm/jobseekerplatform/controller/MainController.java
+++ src/main/java/com/jm/jobseekerplatform/controller/MainController.java
@@ -4,17 +4,18 @@ import com.jm.jobseekerplatform.model.*;
 import com.jm.jobseekerplatform.service.impl.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import javax.persistence.NoResultException;
 import java.util.Base64;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Controller
 public class MainController {
