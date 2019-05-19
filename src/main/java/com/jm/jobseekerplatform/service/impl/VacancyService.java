a5c3ca7722bb299a3ad4ac6a3bcc3fb392ce9598
sopilov <sopilov@list.ru>
2019-05-20 01:11:20 +0300

    add block's and unblock's method's, delete expiry vacancies method's

--- src/main/java/com/jm/jobseekerplatform/service/impl/VacancyService.java
+++ src/main/java/com/jm/jobseekerplatform/service/impl/VacancyService.java
@@ -1,14 +1,15 @@
 package com.jm.jobseekerplatform.service.impl;
 
 import com.jm.jobseekerplatform.dao.impl.VacancyDAO;
+import com.jm.jobseekerplatform.model.State;
 import com.jm.jobseekerplatform.model.Tag;
 import com.jm.jobseekerplatform.model.Vacancy;
 import com.jm.jobseekerplatform.service.AbstractService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Set;
 
 @Service("vacancyService")
@@ -20,5 +21,45 @@ public class VacancyService extends AbstractService<Vacancy> {
 
     public Set<Vacancy> getByTags(Set<Tag> tags, int limit) {
         return  dao.getByTags(tags, limit);
+    }
+
+    public void blockPermanently(Vacancy vacancy){
+        vacancy.setState(State.BLOCK_PERMANENT);
+        dao.update(vacancy);
+    }
+
+    public void blockTemporary (Vacancy vacancy, int periodInDays){
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DATE, periodInDays);
+        Date expiryBlockDate = calendar.getTime();
+
+        vacancy.setState(State.BLOCK_TEMPORARY);
+        vacancy.setExpiryBlock(expiryBlockDate);
+        dao.update(vacancy);
+    }
+
+    public void blockOwn (Vacancy vacancy) {
+        int periodInDays = 30;
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DATE, periodInDays);
+        Date expiryBlockDate = calendar.getTime();
+
+        vacancy.setState(State.BLOCK_OWN);
+        vacancy.setExpiryBlock(expiryBlockDate);
+        dao.update(vacancy);
+    }
+
+    public void unblock (Vacancy vacancy){
+        vacancy.setState(State.ACCESS);
+        vacancy.setExpiryBlock(null);
+        dao.update(vacancy);
+    }
+
+    public int deletePermanentBlockVacancies(){
+        return dao.deletePermanentBlockVacancies();
+    }
+
+    public int deleteExpiryBlockVacancies(){
+        return dao.deleteExpiryBlockVacancies();
     }
 }
