e361dd62d07cfa2e7c339f3586b6d9ee82942fc3
Alex Lemzin <finntroll1488@gmail.com>
2019-05-21 12:10:42 +0300

    Bean import added

--- src/main/java/com/jm/jobseekerplatform/JobseekerPlatformApplication.java
+++ src/main/java/com/jm/jobseekerplatform/JobseekerPlatformApplication.java
@@ -3,6 +3,7 @@ package com.jm.jobseekerplatform;
 import com.jm.jobseekerplatform.config.InitData;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JobseekerPlatformApplication {
