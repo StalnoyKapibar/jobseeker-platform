1e6a868e59c594ca7cd2ff05e119db9a3724b501
sopilov <sopilov@list.ru>
2019-05-23 15:29:41 +0300

    fix mapping and add blockVacancy method

--- src/main/java/com/jm/jobseekerplatform/controller/rest/VacancyRestController.java
+++ src/main/java/com/jm/jobseekerplatform/controller/rest/VacancyRestController.java
@@ -22,19 +22,30 @@ public class VacancyRestController {
         return vacancies;
     }
 
-    @GetMapping("/{vacancyId}")
-    public Vacancy getUserById(@PathVariable Long vacancyId){
+    @RequestMapping("/{vacancyId:\\d+}")
+    public Vacancy getVacancyById(@PathVariable Long vacancyId){
         Vacancy vacancy = vacancyService.getById(vacancyId);
         return vacancy;
     }
 
-    @PutMapping("/{vacancyId}")
+    @RequestMapping(value = "/{vacancyId:\\d+}", method = RequestMethod.PUT)
     @ResponseStatus(HttpStatus.OK)
-    public void updateUser(@PathVariable("vacancyId") Long id,  @RequestBody String state) {
+    public void updateVacancy(@PathVariable("vacancyId") Long id, @RequestBody String state) {
         String st = state.substring(1, state.length() - 1);
         State s = State.valueOf(st);
         Vacancy vacancy = vacancyService.getById(id);
         vacancy.setState(s);
         vacancyService.update(vacancy);
+    }
+
+    @RequestMapping(value = "/block/{vacancyId}", method = RequestMethod.POST)
+    public void blockVacancy(@PathVariable("vacancyId") Long id, @RequestBody int periodInDays) {
+        Vacancy vacancy = vacancyService.getById(id);
+        if (periodInDays == 0){
+            vacancyService.blockPermanently(vacancy);
+        }
+        if (periodInDays > 0 && periodInDays < 15){
+            vacancyService.blockTemporary(vacancy, periodInDays);
+        }
     }
 }
