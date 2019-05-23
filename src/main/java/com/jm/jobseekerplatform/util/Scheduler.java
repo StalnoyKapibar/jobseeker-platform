8a0f3f313943bab05dcfd81297eb4287311dc889
sopilov <sopilov@list.ru>
2019-05-23 17:13:40 +0300

    add deleteExpiryEmployerProfiles method

--- src/main/java/com/jm/jobseekerplatform/util/Scheduler.java
+++ src/main/java/com/jm/jobseekerplatform/util/Scheduler.java
@@ -1,5 +1,6 @@
 package com.jm.jobseekerplatform.util;
 
+import com.jm.jobseekerplatform.service.impl.EmployerProfileService;
 import com.jm.jobseekerplatform.service.impl.VacancyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -13,9 +14,18 @@ public class Scheduler {
     @Autowired
     VacancyService vacancyService;
 
+    @Autowired
+    EmployerProfileService employerProfileService;
+
     @Scheduled(cron = "${scheduler.deleteExpiryVacancies.cron}")
     public void deleteExpiryVacancies() {
         vacancyService.deletePermanentBlockVacancies();
         vacancyService.deleteExpiryBlockVacancies();
+    }
+
+    @Scheduled(cron = "${scheduler.deleteExpiryEmployerProfiles.cron}")
+    public void deleteExpiryEmployerProfiles() {
+        employerProfileService.deletePermanentBlockEmployerProfiles();
+        employerProfileService.deleteExpiryBlockEmployerProfiles();
     }
 }
