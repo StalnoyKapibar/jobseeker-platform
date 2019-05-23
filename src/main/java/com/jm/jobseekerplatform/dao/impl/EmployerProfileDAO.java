72c734ee823eb486bc9feb31660acbe3d6eb9c55
sopilov <sopilov@list.ru>
2019-05-23 17:10:58 +0300

    add block's method's

--- src/main/java/com/jm/jobseekerplatform/dao/impl/EmployerProfileDAO.java
+++ src/main/java/com/jm/jobseekerplatform/dao/impl/EmployerProfileDAO.java
@@ -4,7 +4,21 @@ import com.jm.jobseekerplatform.dao.AbstractDAO;
 import com.jm.jobseekerplatform.model.EmployerProfile;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+
 @Repository("employerProfileDAO")
 public class EmployerProfileDAO extends AbstractDAO<EmployerProfile> {
 
+    public int deletePermanentBlockEmployerProfiles() {
+        int deletedCount = entityManager.createQuery("DELETE FROM EmployerProfile ep WHERE ep.state = 'BLOCK_PERMANENT'").executeUpdate();
+        return deletedCount;
+    }
+
+    public int deleteExpiryBlockEmployerProfiles() {
+        Date currentDate = new Date();
+        int deletedCount = entityManager.createQuery("DELETE FROM EmployerProfile ep WHERE ep.expiryBlock <= :param")
+                .setParameter("param", currentDate)
+                .executeUpdate();
+        return deletedCount;
+    }
 }
--- src/main/java/com/jm/jobseekerplatform/service/impl/EmployerProfileService.java
+++ src/main/java/com/jm/jobseekerplatform/service/impl/EmployerProfileService.java
@@ -2,10 +2,14 @@ package com.jm.jobseekerplatform.service.impl;
 
 import com.jm.jobseekerplatform.dao.impl.EmployerProfileDAO;
 import com.jm.jobseekerplatform.model.EmployerProfile;
+import com.jm.jobseekerplatform.model.State;
 import com.jm.jobseekerplatform.service.AbstractService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Calendar;
+import java.util.Date;
 
 @Service("employerProfileService")
 @Transactional
@@ -14,4 +18,39 @@ public class EmployerProfileService extends AbstractService<EmployerProfile> {
     @Autowired
     private EmployerProfileDAO dao;
 
+    public void blockPermanently(EmployerProfile employerProfile) {
+        employerProfile.setState(State.BLOCK_PERMANENT);
+        employerProfile.setExpiryBlock(null);
+        dao.update(employerProfile);
+    }
+
+    public void blockTemporary(EmployerProfile employerProfile, int periodInDays) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DATE, periodInDays);
+        Date expiryBlockDate = calendar.getTime();
+
+        employerProfile.setState(State.BLOCK_TEMPORARY);
+        employerProfile.setExpiryBlock(expiryBlockDate);
+        dao.update(employerProfile);
+    }
+
+    public void blockOwn(EmployerProfile employerProfile) {
+        employerProfile.setState(State.BLOCK_OWN);
+        employerProfile.setExpiryBlock(null);
+        dao.update(employerProfile);
+    }
+
+    public void unblock(EmployerProfile employerProfile) {
+        employerProfile.setState(State.ACCESS);
+        employerProfile.setExpiryBlock(null);
+        dao.update(employerProfile);
+    }
+
+    public int deletePermanentBlockEmployerProfiles() {
+        return dao.deletePermanentBlockEmployerProfiles();
+    }
+
+    public int deleteExpiryBlockEmployerProfiles() {
+        return dao.deleteExpiryBlockEmployerProfiles();
+    }
 }
