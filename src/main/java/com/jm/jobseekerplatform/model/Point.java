c173b3f01dfea6f002d9f10e2cb252072beedbcc
sopilov <sopilov@list.ru>
2019-05-27 16:01:22 +0300

    add class Point and his Service, DAO

---
+++ src/main/java/com/jm/jobseekerplatform/model/Point.java
@@ -0,0 +1,50 @@
+package com.jm.jobseekerplatform.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "points")
+public class Point implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "lat", nullable = false)
+    private Float latitudeY;
+    @Column(name = "place", nullable = false)
+    private Float longitudeX;
+
+    public Point() {
+    }
+
+    public Point(Float latitudeY, Float longitudeX) {
+        this.latitudeY = latitudeY;
+        this.longitudeX = longitudeX;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Float getLatitudeY() {
+        return latitudeY;
+    }
+
+    public void setLatitudeY(Float latitudeY) {
+        this.latitudeY = latitudeY;
+    }
+
+    public Float getLongitudeX() {
+        return longitudeX;
+    }
+
+    public void setLongitudeX(Float longitudeX) {
+        this.longitudeX = longitudeX;
+    }
+}
