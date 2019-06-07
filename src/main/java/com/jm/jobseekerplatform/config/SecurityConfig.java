15bab99f2db80d887aad31d81728c84980cdd6d5
Alex Lemzin <finntroll1488@gmail.com>
2019-06-07 20:07:23 +0300

    added login successhandler

--- src/main/java/com/jm/jobseekerplatform/config/SecurityConfig.java
+++ src/main/java/com/jm/jobseekerplatform/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.jm.jobseekerplatform.config;
 
+import com.jm.jobseekerplatform.service.impl.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -21,6 +22,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDetailsService userDetailsService;
 
+    @Autowired
+    private UserService userService;
+
     private PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
 
 
@@ -40,6 +44,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.formLogin()
                 // указываем страницу с формой логина
                 .loginPage("/login")
+                //указываем логику обработки при логине
+                .successHandler(new LoginSuccessHandler(userService))
                 // указываем action с формы логина
                 .loginProcessingUrl("/login")
                 // указываем URL при неудачном логине
