f204eaad38099daca05312c12bf80cd5ed8b16b9
Alex Lemzin <finntroll1488@gmail.com>
2019-06-07 19:57:14 +0300

    added seekers js

--- src/main/resources/static/js/admin_seekers.js
+++ src/main/resources/static/js/admin_seekers.js
@@ -1,50 +1,136 @@
 $(document).ready(function () {
-    getSeekers();
+    'use strict';
+    feather.replace();
+    $('#viewBy option').each(function () {
+        var param = $(this);
+        if (location.href.indexOf(param.val()) !== -1) {
+            param.prop('selected', true);
+        }
+    });
+    $('#sorBy option').each(function () {
+        var param = $(this);
+        if (location.href.indexOf(param.val()) !== -1) {
+            param.prop('selected', true);
+        }
+    });
+
 });
 
-function getSeekers() {
-    $.ajax({
-        url: '/api/seekerprofiles',
-        type: 'GET',
-        success: function (list) {
-            var trHTML = '';
-            $.each(list, function (i, item) {
-                trHTML += '<tr>' +
-                    '<td>' + item.id + '</td>' +
-                    '<td>' + item.email + '</td>' +
-                    '<button onclick="viewSeeker(' + item.seekerProfile.id + ')" data-toggle="modal" data-target="#seekerProfileModal" class="btn btn-primary seekerProfileModal">View Profile</button> ' +
-                    '<td>' + printTags(item.roles) + '</td>' +
-                    '<td>' + printPortfolios(item.roles) + '</td>' +
-                    '<td>' +
-                    '<button onclick="editSeeker(' + item.id + ')" data-toggle="modal" data-target="#editSeekerModal" class="btn btn-primary editSeekerModal">Edit User</button> ' +
-                    '<button onclick="editSeekerProfile(' + item.seekerProfile.id + ')"  data-toggle="modal" data-target="#editSeekerProfileModal" class="btn btn-primary editSeekerProfileModal">Edit Profile</button>' +
-                    '</td>' +
-                    '</tr>';
-            });
+$("#viewBy").change(function () {
+    var size = $(this).val();
+    var direction = $("#sorBy option:selected").val();
+    location.href = '/admin/seekers?size=' + size + '&direction=' + direction;
+});
 
-            $('tbody').html(trHTML);
+$("#sorBy").change(function () {
+    var direction = $(this).val();
+    var size = $("#viewBy option:selected").val();
+    location.href = '/admin/seekers?size=' + size + '&direction=' + direction;
+});
+
+$(function () {
+    $('#pagList a').attr('href', function (index, href) {
+        var size = $("#viewBy option:selected").val();
+        var direction = $("#sorBy option:selected").val();
+        return href + '&size=' + size + '&direction=' + direction;
+    });
+});
+
+
+function editSeeker(id) {
+    location.href = '/admin/seeker/' + id
+}
+
+$('#editSeekerForm').click(function (event) {
+    event.preventDefault();
+    var token = $("meta[name='_csrf']").attr("content");
+    var header = $("meta[name='_csrf_header']").attr("content");
+
+    var id = $('#editSeeker').find("input[name='id']").val();
+    var email = $('#editSeeker').find("input[name='email']").val();
+    var password = $('#editSeeker').find("input[name='password']").val();
+    var date = $('#editSeeker').find("input[name='date']").val();
+    var authorityId = $('#editSeeker').find("input[name='authorityId']").val();
+    var authority = $('#editSeeker').find("input[name='authority']").val();
+    var enabled = $('#editSeeker').find("input[name='enabled']").val();
+    var confirm = $('#editSeeker').find("input[name='confirm']").val();
+
+
+    var profId = $('#editSeeker').find("input[name='profId']").val();
+    var name = $('#editSeeker').find("input[name='name']").val();
+    var patronymic = $('#editSeeker').find("input[name='patronymic']").val();
+    var surname = $('#editSeeker').find("input[name='surname']").val();
+    var description = $('#editSeeker').find("input[name='description']").val();
+    //var photo = $('#seekerPhoto img').attr('src');
+
+    var userAuthority = {
+        'id': authorityId,
+        'authority': authority
+    };
+
+
+    var seekerProfile = {
+        'id': profId,
+        'name': name,
+        'patronymic': patronymic,
+        'surname': surname,
+        'description': description
+    };
+
+    var seeker = {
+        'id': id,
+        'email': email,
+        'password': password,
+        'date': date,
+        'authority': userAuthority,
+        'enabled': enabled,
+        'confirm': confirm,
+        'seekerProfile': seekerProfile
+    };
+
+    $.ajax({
+        url: '/api/seeker/edit',
+        contentType: 'application/json; charset=utf-8',
+        type: 'POST',
+        beforeSend: function (request) {
+            request.setRequestHeader(header, token);
+        },
+        data: JSON.stringify(seeker),
+        success: function () {
+            location.href = '/admin/seekers'
+        },
+        error: function (error) {
+            console.log(error);
+            alert(error.toString());
+        }
+    });
+});
+
+function uploadPhoto() {
+
+    var id = $('#editPhotoForm').find("input[name='seekerId']").val();
+    var photo = $('#editPhotoForm').find("input[name='file']").val();
+
+    $.ajax({
+        url: '/api/seeker/editPhoto',
+        type: 'POST',
+        data: {photo: photo},
+        success: function () {
+            location.href = '/admin/seeker/' + id
         }
     });
 }
 
-function viewSeeker(id) {
-    $.ajax({
-        url: '/api/seekerprofiles/' + id,
-        type: 'GET',
-        success: function (seeker) {
-            var id = seeker.id;
-            var name = seeker.name;
-            var patronymic = seeker.patronymic;
-            var surname = seeker.surname;
-            var description = seeker.description;
-            var photo = seeker.photo;
+/*function editPhoto(id) {
 
-            $("#seekerProfileModal").find("input[name='Id']").val(id);
-            $("#seekerProfileModal").find("input[name='Name']").val(name);
-            $("#seekerProfileModal").find("input[name='Patronymic']").val(patronymic);
-            $("#seekerProfileModal").find("input[name='Surname']").val(surname);
-            $("#seekerProfileModal").find("input[name='Description']").val(description);
-            $("#seekerProfileModal").find("input[name='Photo']").val(photo);
+}*/
+
+function deleteSeeker(id) {
+    $.ajax({
+        url: '/api/seeker/delete/' + id,
+        type: 'GET',
+        success: function () {
+            location.href = '/admin/seekers'
         }
     });
 }
