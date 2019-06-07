c1e16e29ac0ad8d7d1f5a6fe07b6ff96e95d6185
Alex Lemzin <finntroll1488@gmail.com>
2019-06-07 20:06:13 +0300

    separated methods to different classes

--- src/main/java/com/jm/jobseekerplatform/controller/MainController.java
+++ src/main/java/com/jm/jobseekerplatform/controller/MainController.java
@@ -3,15 +3,19 @@ package com.jm.jobseekerplatform.controller;
 import com.jm.jobseekerplatform.model.*;
 import com.jm.jobseekerplatform.service.impl.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
 import javax.persistence.NoResultException;
+import javax.servlet.http.HttpServletRequest;
 import java.util.Base64;
 import java.util.List;
 import java.util.Set;
@@ -63,45 +67,6 @@ public class MainController {
         return "index";
     }
 
-    @RequestMapping("/employer/{employerProfileId}")
-    public String employerProfilePage(@PathVariable Long employerProfileId, Model model, Authentication authentication) {
-        EmployerProfile employerProfile = employerProfileService.getById(employerProfileId);
-        model.addAttribute("eprofile", employerProfile);
-        model.addAttribute("logoimg", Base64.getEncoder().encodeToString(employerProfile.getLogo()));
-        if (authentication != null && authentication.isAuthenticated()) {
-            Set<String> roles = authentication.getAuthorities().stream().map(grantedAuthority -> ((GrantedAuthority) grantedAuthority).getAuthority()).collect(Collectors.toSet());
-            if (roles.contains("ROLE_SEEKER") | roles.contains("ROLE_ADMIN")) {
-                Long id = ((User) authentication.getPrincipal()).getId();
-                if (roles.contains("ROLE_SEEKER")) {
-                    Seeker seeker = (Seeker) seekerService.getById(id);
-                    model.addAttribute("seekerId", seeker.getSeekerProfile().getId());
-                }
-            }
-            if (!employerProfile.getReviews().isEmpty()) {
-                Set<EmployerReviews> employerReviews = employerProfile.getReviews();
-                if (employerReviews.size() < 2) {
-                    model.addAttribute("minReviewsEvaluation", employerReviews.iterator().next());
-                } else if (employerReviews.size() >= 2) {
-                    model.addAttribute("minReviewsEvaluation", employerReviews.stream().sorted().skip(employerReviews.size() - 1).findFirst().orElse(null));
-                    model.addAttribute("maxReviewsEvaluation", employerReviews.stream().sorted().findFirst().orElse(null));
-                } else {
-                    model.addAttribute("reviewStatus", false);
-                }
-            } else {
-                model.addAttribute("reviewStatus", false);
-            }
-        }
-        return "employer";
-    }
-
-    @RequestMapping("/seeker/{seekerProfileId}")
-    public String seekerProfilePage(@PathVariable Long seekerProfileId, Model model) {
-        SeekerProfile seekerProfile = seekerProfileService.getById(seekerProfileId);
-        model.addAttribute("sprofile", seekerProfile);
-        model.addAttribute("photoimg", Base64.getEncoder().encodeToString(seekerProfile.getPhoto()));
-        return "seeker";
-    }
-
     @RequestMapping("/admin")
     public String adminPage() {
         return "admin";
@@ -120,7 +85,6 @@ public class MainController {
         model.addAttribute("logout", logout != null);
         return "login";
     }
-
 
     @RequestMapping(value = "/registration", method = RequestMethod.GET)
     public String reg() {
