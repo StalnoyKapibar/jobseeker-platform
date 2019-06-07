752fde398d75c527096e137f9628f682ea71a03d
Alex Lemzin <finntroll1488@gmail.com>
2019-06-07 20:09:12 +0300

    created seeker dao interfase

--- src/main/java/com/jm/jobseekerplatform/dao/SeekerDaoI.java
+++ src/main/java/com/jm/jobseekerplatform/dao/SeekerDaoI.java
@@ -1,4 +1,12 @@
 package com.jm.jobseekerplatform.dao;
 
-public interface SeekerDaoI {
+import com.jm.jobseekerplatform.model.Seeker;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SeekerDaoI extends JpaRepository<Seeker,Long> {
+    Page<Seeker> findAll(Pageable pageable);
 }
