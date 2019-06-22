ae044a563886ad50931b18401698e08bb7831213
Aytalina Malysheva <aita.m@bk.ru>
2019-06-22 20:11:15 +0300

    В TegRestController добавлен метод получения тегов по id соискателя

--- src/main/java/com/jm/jobseekerplatform/controller/rest/TagRestController.java
+++ src/main/java/com/jm/jobseekerplatform/controller/rest/TagRestController.java
@@ -1,12 +1,15 @@
 package com.jm.jobseekerplatform.controller.rest;
 
 import com.jm.jobseekerplatform.model.Tag;
+import com.jm.jobseekerplatform.service.impl.SeekerProfileService;
 import com.jm.jobseekerplatform.service.impl.TagService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/api/tags")
@@ -15,9 +18,16 @@ public class TagRestController {
     @Autowired
     private TagService tagService;
 
+    @Autowired
+    private SeekerProfileService seekerProfileService;
+
     @RequestMapping("/")
     public List<Tag> getAllTags() {
-        List<Tag> tags = tagService.getAll();
-        return tags;
+        return tagService.getAll();
+    }
+
+    @RequestMapping("/seeker/{seeker_id}")
+    public Set<Tag> getSeekerTags(@PathVariable("seeker_id") String seekerId) {
+        return seekerProfileService.getById(Long.parseLong(seekerId)).getTags();
     }
 }
