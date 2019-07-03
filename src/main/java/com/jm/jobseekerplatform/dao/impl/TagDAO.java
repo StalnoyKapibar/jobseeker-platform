4d3303bc39a4a110717377ed4075f89207983bba
Alex Lemzin <finntroll1488@gmail.com>
2019-07-03 21:34:56 +0300

    Добавлен метод для поиска тега по части имени и добавлен блок try/catch в метод findByName

--- src/main/java/com/jm/jobseekerplatform/dao/impl/TagDAO.java
+++ src/main/java/com/jm/jobseekerplatform/dao/impl/TagDAO.java
@@ -4,13 +4,29 @@ import com.jm.jobseekerplatform.dao.AbstractDAO;
 import com.jm.jobseekerplatform.model.Tag;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository("tagDAO")
 public class TagDAO extends AbstractDAO<Tag> {
 
     public Tag findByName(String name) {
+       Tag tag = null;
+        try {
+            tag =  entityManager
+                    .createQuery("SELECT t FROM Tag t WHERE t.name = :param", Tag.class)
+                    .setParameter("param", name)
+                    .getSingleResult();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return tag;
+    }
+
+    public List<Tag> getBySearchParam(String param) {
+        String param1 = "%" + param + "%";
         return entityManager
-                .createQuery("SELECT t FROM Tag t WHERE t.name = :param", Tag.class)
-                .setParameter("param", name)
-                .getSingleResult();
+                .createQuery("SELECT t FROM Tag t WHERE t.name like :param", Tag.class)
+                .setParameter("param", param1)
+                .getResultList();
     }
 }
