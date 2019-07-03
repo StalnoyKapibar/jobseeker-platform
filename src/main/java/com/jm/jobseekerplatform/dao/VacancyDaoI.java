94dc2395ac41217ac71731033ca67f799f2d000e
Alex Lemzin <finntroll1488@gmail.com>
2019-07-03 21:39:55 +0300

    Добавлен интерфейс для поиска вакансий по тегам и реализации постраничной пагинации

--- src/main/java/com/jm/jobseekerplatform/dao/VacancyDaoI.java
+++ src/main/java/com/jm/jobseekerplatform/dao/VacancyDaoI.java
@@ -1,4 +1,19 @@
-package com.jm.jobseekerplatform.dao.impl;
+package com.jm.jobseekerplatform.dao;
 
-public interface VacancyDaoI {
+import com.jm.jobseekerplatform.model.Tag;
+import com.jm.jobseekerplatform.model.Vacancy;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Set;
+
+@Repository
+public interface VacancyDaoI extends JpaRepository<Vacancy,Long> {
+    Page<Vacancy> findAll(Pageable pageable);
+
+    @Query(value = "SELECT distinct v FROM Vacancy v JOIN v.tags t WHERE t IN ?1")
+    Page<Vacancy> findAllByTags(Set<Tag> tags, Pageable pageable);
 }
