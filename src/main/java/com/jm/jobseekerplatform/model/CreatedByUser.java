6919e62596891d8e8518ad154e0137041d52f154
Nikolay Dolgopolov <nickolanick26@gmail.com>
2019-07-03 12:55:01 +0300

    - fix interface.

--- src/main/java/com/jm/jobseekerplatform/model/CreatedByUser.java
+++ src/main/java/com/jm/jobseekerplatform/model/CreatedByUser.java
@@ -4,5 +4,5 @@ package com.jm.jobseekerplatform.model;
  * @author Nick Dolgopolov (nick_kerch@mail.ru; https://github.com/Absent83/)
  */
 public interface CreatedByUser {
-    User getUser();
+    //User getUser();
 }
