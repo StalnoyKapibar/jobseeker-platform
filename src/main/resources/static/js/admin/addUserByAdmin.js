4199d6c8277d322ec5707ccb3774e800d0d40de3
ZuiQuanZ <123casper@mail.ru>
2019-07-08 23:45:44 +0300

    admin page ready for bootstrap4(no)

--- src/main/resources/static/js/admin/addUserByAdmin.js
+++ src/main/resources/static/js/admin/addUserByAdmin.js
@@ -1,58 +1,87 @@
-$(document).ready(function () {
+var email_check = false;
+var pass_check = false;
+var pass_conf_check = false;
+var role_check = false;
 
+$(document).ready(function () {
     feather.replace();
     bootstrapValidate('#user_email', 'email:Введите корректный Email-адрес', function (isValid) {
         if (isValid) {
             exists();
         } else {
             $('#exists_warning').html('');
+            email_check = false;
         }
     })
     bootstrapValidate('#user_password', 'regex:^(?=.*[a-z].*)(?=.*[0-9].*)[A-Za-z0-9]{6,20}$:Пароль должен содержать латинские буквы и цифры, от 6 до 20 символов', function (isValid) {
         if (isValid) {
             $('#user_password').addClass('is-valid');
+            pass_check = true;
         } else {
+            pass_check = false;
+        }
+        if ($('#user_password').val() == $('#confirm_password').val()) {
+            passConfirm(true)
+        } else {
+            passConfirm(false)
+        }
+    })
+    bootstrapValidate('#confirm_password', 'matches:#user_password:', function (isValid) {
+        passConfirm(isValid);
+    })
 
-        }
-    })
-    bootstrapValidate('#confirm_password', 'matches:#user_password:Пароли не совпадают', function (isValid) {
+    function passConfirm(isValid) {
         if (isValid) {
+            $('#confirm_password').removeClass('is-invalid');
             $('#confirm_password').addClass('is-valid');
+            pass_conf_check = true;
+        } else {
+            $('#confirm_password').addClass('is-invalid');
+            pass_conf_check = false;
         }
-    })
+    }
+
     bootstrapValidate('#role', 'required:Нужно выбрать роль', function (isValid) {
         if (isValid) {
             $('#role').addClass('is-valid');
+            role_check = true;
+        } else {
+            role_check = false;
         }
     })
 })
 
-// }
-// (function () {
-//     'use strict';
-//     window.addEventListener('load', function () {
-//         var form = document.getElementById('contact_form');
-//         form.addEventListener('submit', function (event) {
-//             if (form.checkValidity() === false) {
-//                 alert("Форма заполнена не верно");
-//                 event.preventDefault();
-//                 event.stopPropagation();
-//             } else {
-//                 addUser();
-//             }
-//             form.classList.add('was-validated');
-//         }, false);
-//     }, false);
-// })();
+function alertForm() {
+    if (email_check && pass_check && pass_conf_check && role_check) {
+        $('#ups_message').hide();
+    }
+    $('#success_message').hide();
+}
+
+function notifyUser() {
+    if (document.getElementById('notify').checked) {
+        $('#notify').addClass("is-valid");
+    } else {
+        $('#notify').removeClass("is-valid");
+    }
+}
+
+function validityForm() {
+    if (email_check && pass_check && pass_conf_check && role_check) {
+        addUser();
+    } else {
+        $('#ups_message').slideDown({opacity: "show"}, "slow");
+    }
+}
+
 
 function addUser() {
-
     var token = $("meta[name='_csrf']").attr("content");
     var header = $("meta[name='_csrf_header']").attr("content");
     var userEmail = $("#user_email").val();
     var userPass = $("#user_password").val();
     var role = {'authority': $("#role").val()};
-    var checkBox = document.getElementById("myCheck");
+    var checkBox = document.getElementById("notify");
 
     var newUser = {
         'email': userEmail,
@@ -73,14 +102,19 @@ function addUser() {
                 console.log(data);
                 $('#email_ver').text(userEmail);
                 $('#success_message').slideDown({opacity: "show"}, "slow");
+                $('#ups_message').hide();
                 $('#contact_form')[0].reset();
-                $('#contact_form').data('bootstrapValidator').resetForm();
+                $('#user_email').removeClass('is-valid');
+                $('#user_password').removeClass('is-valid');
+                $('#confirm_password').removeClass('is-valid');
+                $('#notify').removeClass('is-valid');
+                $('#role').removeClass('is-valid');
             },
         error: function (error) {
             console.log(error);
             alert(error.responseJSON.message);
         }
-    });
+    })
 }
 
 function exists() {
@@ -91,8 +125,10 @@ function exists() {
             if (result) {
                 $('#user_email').addClass("is-invalid");
                 $('#exists_warning').html("Email-адрес уже существует");
+                email_check = false;
             } else {
                 $('#user_email').addClass("is-valid");
+                email_check = true;
             }
         }
     })
