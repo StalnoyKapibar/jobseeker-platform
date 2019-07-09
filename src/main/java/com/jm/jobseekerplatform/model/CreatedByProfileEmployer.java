dbe35b7657a011de4aff68df360c64f5aec97d4a
Nikolay Dolgopolov <nickolanick26@gmail.com>
2019-07-10 00:28:32 +0300

    - move admin html templates to the folder "admin"

--- src/main/java/com/jm/jobseekerplatform/model/CreatedByProfileEmployer.java
+++ src/main/java/com/jm/jobseekerplatform/model/CreatedByProfileEmployer.java
@@ -5,6 +5,5 @@ import com.jm.jobseekerplatform.model.profiles.ProfileEmployer;
 /**
  * @author Nick Dolgopolov (nick_kerch@mail.ru; https://github.com/Absent83/)
  */
-public interface CreatedByProfileEmployer extends CreatedByProfile<ProfileEmployer>
-{
+public interface CreatedByProfileEmployer extends CreatedByProfile<ProfileEmployer> {
 }
