7cc99d090fb61a6cef285d2a766602b86a216e75
Aytalina Malysheva <aita.m@bk.ru>
2019-07-26 13:53:48 +0300

    в CityDAO добавлен метод поиска города по имени

--- src/main/java/com/jm/jobseekerplatform/dao/impl/CityDAO.java
+++ src/main/java/com/jm/jobseekerplatform/dao/impl/CityDAO.java
@@ -7,4 +7,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class CityDAO extends AbstractDAO<City> {
 
+    public City findCityByName(String name) {
+        return (City)entityManager.createQuery("select c from City c where c.name=:city").setParameter("city", name).getSingleResult();
+    }
+
 }
