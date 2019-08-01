d2e5ace3d9042efafc88080060b4a95e207f6de5
elendiD <elendidandy@gmail.com>
2019-08-01 23:28:54 +0300

    добавил отображение вакансий при запрете отслеживания геопозиции

--- src/main/java/com/jm/jobseekerplatform/controller/rest/VacancyRestController.java
+++ src/main/java/com/jm/jobseekerplatform/controller/rest/VacancyRestController.java
@@ -1,5 +1,6 @@
 package com.jm.jobseekerplatform.controller.rest;
 
+import com.jm.jobseekerplatform.dto.VacancyPageDTO;
 import com.jm.jobseekerplatform.model.*;
 import com.jm.jobseekerplatform.model.profiles.EmployerProfile;
 import com.jm.jobseekerplatform.model.profiles.SeekerProfile;
@@ -18,6 +19,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.security.RolesAllowed;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -89,7 +91,7 @@ public class VacancyRestController {
         if (searchParam.isEmpty()) {
             return new ResponseEntity<>(new HashSet<>(), HttpStatus.OK);
         }
-        List<Vacancy> list = vacancyService.findAllByTags(searchParam,PageRequest.of(pageCount, 10,
+        List<Vacancy> list = vacancyService.findAllByTags(searchParam, PageRequest.of(pageCount, 10,
                 new Sort(Sort.Direction.ASC, "id"))).getContent();
         return new ResponseEntity<>(new HashSet<>(list), HttpStatus.OK);
     }
@@ -98,10 +100,16 @@ public class VacancyRestController {
     public Page<Vacancy> getPageOfVacancies(@RequestBody Point point, @RequestParam("city") String city, @PathVariable("page") int page, Authentication authentication) {
         int limit = 10;
         if (authentication == null || !authentication.isAuthenticated()) {
-            return vacancyService.findVacanciesByPoint(city, point, limit, page);
+            if (city.equals("undefined")) {
+                List<Vacancy> all = vacancyService.getAll();
+                Collections.shuffle(all);
+                return new VacancyPageDTO(all.subList(0, limit), page);
+            } else {
+                return vacancyService.findVacanciesByPoint(city, point, limit, page);
+            }
         } else {
             if (authentication.getAuthorities().contains(roleSeeker)) {
-                Set<Tag> tags = ((SeekerProfile)((User)authentication.getPrincipal()).getProfile()).getTags();
+                Set<Tag> tags = ((SeekerProfile) ((User) authentication.getPrincipal()).getProfile()).getTags();
                 return vacancyService.findVacanciesByTagsAndByPoint(city, point, tags, limit, page);
             }
         }
