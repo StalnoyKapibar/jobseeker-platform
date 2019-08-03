1e988224be30cd641bd3a891cf08db57adb48e8d
Aytalina Malysheva <aita.m@bk.ru>
2019-08-03 15:11:16 +0300

    correct

--- src/main/java/com/jm/jobseekerplatform/dao/VacancyDaoI.java
+++ src/main/java/com/jm/jobseekerplatform/dao/VacancyDaoI.java
@@ -1,15 +1,12 @@
 package com.jm.jobseekerplatform.dao;
 
-import com.jm.jobseekerplatform.model.Tag;
 import com.jm.jobseekerplatform.model.Vacancy;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.Set;
 
 @Repository
 public interface VacancyDaoI extends JpaRepository<Vacancy,Long> {
@@ -17,7 +14,4 @@ public interface VacancyDaoI extends JpaRepository<Vacancy,Long> {
     @EntityGraph(value = "vacancy-all-nodes" , type= EntityGraph.EntityGraphType.FETCH)
     Page<Vacancy> findAll(Pageable pageable);
 
-//    @EntityGraph(value = "vacancy-all-nodes" , type= EntityGraph.EntityGraphType.FETCH)
-//    @Query(value = "SELECT distinct v FROM Vacancy v JOIN v.tags t WHERE t IN ?1")
-//    Page<Vacancy> findAllByTags(Set<Tag> tags, Pageable pageable);
 }
