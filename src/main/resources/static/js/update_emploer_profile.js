584d47c1ee3659202330442dc5479e5e8d4194a9
vladislavsemenenko <tresh@vsemenenko.com>
2019-08-08 22:59:11 +0300

    закончил с статичной страницей эмплоерпрофала

--- src/main/resources/static/js/update_emploer_profile.js
+++ src/main/resources/static/js/update_emploer_profile.js
@@ -23,12 +23,28 @@ $( document ).ready(function() {
     $("#trckVacancies, #allVacancies").sortable({
         connectWith: "#trckVacancies, #allVacancies",
         update: function() {
-            var sort = [];
-            $('#card_body_1').each(function () {
-                sort.push($(this).text());
+            var vacancies = [];
+            $('#trckVacancies span').each(function () {
+                vacancies.push($(this).data('id'));
             });
-            var saveSort = {'sort':sort};
-            sessionStorage.setItem('sort', JSON.stringify(saveSort));
+            var saveSort = {'vacancies':vacancies};
+            sessionStorage.setItem('vacancies', JSON.stringify(saveSort));
         }
     });
+
+    $(".custom-file-input").on("change", function() {
+        var fileName = $(this).val().split("\\").pop();
+        $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+    });
+
 });
+
+function update(id) {
+    var companyName = document.getElementById("companyname").value;
+    var site = document.getElementById("companywebsite").value;
+    var description = document.getElementById("description").value;
+    var vacansies = JSON.parse(sessionStorage.getItem('vacancies'));
+    alert(companyName);
+
+}
+
