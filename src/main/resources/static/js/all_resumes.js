013aaf550bdf66d58b339612d35912ead156f026
Andrei Dmitrichenko <helvetto@gmail.com>
2019-08-16 10:37:27 +0300

    Сделал временную заглушку, вместо резюме отображаются вакансии

--- src/main/resources/static/js/all_resumes.js
+++ src/main/resources/static/js/all_resumes.js
@@ -1,6 +1,9 @@
 var token = $("meta[name='_csrf']").attr("content");
 var header = $("meta[name='_csrf_header']").attr("content");
 
+var page = 1;
+var total_pages;
+
 $(function () {
     $('#search_box').bind("input keyup click", function () {
         if (this.value == '') {
@@ -67,5 +70,178 @@ function deleteButton(id) {
 }
 
 function searchResults() {
-   alert('This feature will work later!')
+    blockScroll = true;
+    $("#empCardVac").remove();
+    $('#searchList').remove();
+    $('#searchResult').append('<ul id="searchList" class="list-group"></ul>');
+    var param = [];
+    $('.listTags').each(function (key, value) {
+        var tagId = $(this).find('.tagIdH').val();
+        var tagName = $(this).find('.tagButton').text();
+        var tag = {
+            'id': tagId,
+            'name': tagName
+        };
+        param.push(tag);
+    });
+    var searchBox = $('#search_box').val();
+    if (param.length === 0 && searchBox.length === 0) {
+        $('#searchHeader').text('Задан пустой запрос');
+    } else {
+        var pageCount = 0;
+        $('#isAVShow').val('1');
+        $.ajax({
+            type: 'post',
+            url: "api/vacancies/search?pageCount=" + pageCount,
+            contentType: 'application/json; charset=utf-8',
+            beforeSend: function (request) {
+                request.setRequestHeader(header, token);
+            },
+            data: JSON.stringify(param),
+            success: function (data) {
+                if (data.content.length === 0) {
+                    if (searchBox.length === 0 && param.length > 0) {
+                        $('#searchHeader').text('По запросу "' + param[0].name + '" ничего не найдено');
+                    } else {
+                        $('#searchHeader').text('По запросу "' + searchBox + '" ничего не найдено');
+                    }
+                } else {
+                    if (param.length === 1) {
+                        $('#searchHeader').text('Резюме по тегу :');
+                    } else {
+                        $('#searchHeader').text('Резюме по тегам :');
+                    }
+                    printResumes(data.content);
+                    total_pages = data.totalPages;
+                    pageCount++;
+                    $('#scrollPageCount').val(pageCount);
+                }
+            },
+            error: function (error) {
+                console.log(error);
+                alert(error.toString());
+            }
+        })
+    }
+}
+
+function printResumes(data) {
+    $.each(data, function (key, value) {
+        var favVac = '';
+        var vacancyTags = '';
+        var minSalary = '';
+        if (value.salaryMin) {
+            minSalary = '<div class="salary"><span>Зарплата от: ' + value.salaryMin + ' руб.</span></div>';
+        }
+        $.each(value.tags, function (i, item) {
+            vacancyTags += '<span class="badge badge-pill badge-success btnClick text-dark" style="white-space: pre"><h7>' + item.name + '   </h7></span>';
+
+        });
+        $('#searchList').append('<li class="list-group-item clearfix" data-toggle="modal"' +
+            ' data-target="#vacancyModal" onclick="showVacancy(\'' + value.id + '\')">' +
+            '<div class="headLine"><span>' + value.headline + '</span>' + '   ' + favVac + '</div>' +
+            '<div class="vacancyTags">' + vacancyTags + '</div>' +
+            '<div class="companyData"><span>Компания: ' + value.creatorProfile + '</span><br><span>Город: ' + value.city + '</span></div>' +
+            '<div class="vacancyDescription"><span>' + value.shortDescription + '</span></div>' +
+            minSalary +
+            '<div class="pull-right">' +
+            '<span class="btn btn-outline-success btn-sm btnOnVacancyPage"' +
+            'onclick="window.location.href =\'/vacancy/' + value.id + '\';event.stopPropagation();">На страницу вакансии</span>' + '</div>' +
+            '</li>');
+    });
+}
+
+function showVacancy(id) {
+    $.ajax({
+        url: "/api/vacancies/" + id,
+        type: "GET",
+        async: false,
+        success: function (data) {
+            var tags = "";
+            $.each(data.tags, function (key, value) {
+                tags += '<span class="badge badge-pill badge-success btnClick text-dark">' + value.name + '</span>'
+            });
+            $("#VMTags").html(tags);
+            $("#VMHeadline").text(data.headline);
+            $("#VMCity").text(data.city);
+            $("#VMDescription").html(data.description);
+
+            var str = "Зарплата: ";
+
+            if (data.salaryMin != null) {
+                str = str + "от " + data.salaryMin + " рублей ";
+            }
+            if (data.salaryMax != null) {
+                str = str + "до " + data.salaryMax + " рублей";
+            }
+            if (str == "Зарплата: ") {
+                str = "Зарплата не указана";
+            }
+            $("#VMSalary").text(str);
+
+            if (data.remote) {
+                $('#VMRemote').show();
+            } else {
+                $('#VMRemote').hide();
+            }
+
+        }
+    });
+}
+
+$(window).scroll(function () {
+    if ($(document).height() - $(window).height() === $(window).scrollTop()) {
+        if (block = true) {
+            if (page < total_pages) {
+                if (blockScroll == false) {
+                    getSortedVac(point);
+                }
+            } else if (page == total_pages) {
+                if (blockScroll == false) {
+                    getSortedVac(point);
+                }
+            }
+        }
+        if ($('#search_box').val() === '' && $('#isAVShow').val() === '0') {
+            return;
+        } else {
+            var param = [];
+            $('.listTags').each(function (key, value) {
+                var tagId = $(this).find('.tagIdH').val();
+                var tagName = $(this).find('.tagButton').text();
+                var tag = {
+                    'id': tagId,
+                    'name': tagName
+                };
+                param.push(tag);
+            });
+            var pageCount = $('#scrollPageCount').val();
+            if (pageCount < total_pages - 1) {
+                searchVac(pageCount, param)
+            } else if (pageCount == total_pages - 1) {
+                searchVac(pageCount, param);
+            }
+        }
+    }
+});
+
+function searchVac(pageCount, param) {
+    $.ajax({
+        type: 'post',
+        url: "api/vacancies/search?pageCount=" + pageCount,
+        contentType: 'application/json; charset=utf-8',
+        beforeSend: function (request) {
+            request.setRequestHeader(header, token);
+        },
+        data: JSON.stringify(param),
+        success: function (data) {
+            printResumes(data.content);
+            pageCount++;
+            $('#scrollPageCount').val(pageCount);
+        },
+        error: function (error) {
+            console.log(error);
+            alert(error.toString());
+        }
+    });
 }
