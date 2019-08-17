b11f7c2955255651fe6ecb28ade21f2346bce2b9
elendiD <elendidandy@gmail.com>
2019-08-17 19:11:54 +0300

    добавил аспект для счётчика просмотров новостей.
    новость считается просмотренной каждый раз когда запрос успешно отправляется пользователю.

---
+++ src/main/java/com/jm/jobseekerplatform/aop/NewsCounter.java
@@ -0,0 +1,42 @@
+package com.jm.jobseekerplatform.aop;
+
+import com.jm.jobseekerplatform.model.News;
+import com.jm.jobseekerplatform.service.impl.NewsService;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+
+@Aspect
+@Component
+public class NewsCounter {
+
+    @Autowired
+    private NewsService newsService;
+
+    @Pointcut("execution(* getAllNewsBySeekerProfileId(..))")
+    private void countNews() {
+    }
+
+    @AfterReturning(
+            pointcut = "com.jm.jobseekerplatform.aop.NewsCounter.countNews()",
+            returning = "retVal")
+    public void doAccessCheck(Object retVal) {
+        ResponseEntity responseEntity = (ResponseEntity) retVal;
+        Collection entityBody = (Collection) responseEntity.getBody();
+
+        entityBody.forEach(x -> {
+            News news = (News) x;
+            Long totalViews = news.getNumberOfViews();
+            if (totalViews == null) {
+                totalViews = 0L;
+            }
+            news.setNumberOfViews(totalViews + 1);
+            newsService.update(news);
+        });
+    }
+}
