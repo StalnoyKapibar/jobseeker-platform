537391e1585b81c99b3ca2e44fe0e2742f92fdf5
Andrei Dmitrichenko <helvetto@gmail.com>
2019-08-22 19:52:08 +0300

    Оптимизировал запрос на получение резюме по тегам

--- src/main/java/com/jm/jobseekerplatform/dao/impl/ResumeDAO.java
+++ src/main/java/com/jm/jobseekerplatform/dao/impl/ResumeDAO.java
@@ -8,7 +8,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.Query;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -21,45 +20,27 @@ public class ResumeDAO extends AbstractDAO<Resume> {
         Query query = entityManager.createQuery("select r from Resume r", Resume.class);
 
         long totalElements = (long) entityManager.createQuery("select count(r) from Resume r").getSingleResult();
-        int totalPages = (int) (Math.ceil((double) totalElements / (double) limit));
-
+        int totalPages = getTotalPages(totalElements, limit);
         List<Resume> resumes = query.setFirstResult(page * limit).setMaxResults(limit).getResultList();
 
         return new ResumePageDTO(resumes, totalPages);
     }
 
     public Page<Resume> getResumesByTag(Set<Tag> tags, int limit, int page) {
-        Query query = entityManager.createQuery("select v.id, count(v.id) as c from Resume v join v.tags t where t in (:tags) group by (v.id) order by c desc")
+        Query query = entityManager.createQuery("select v from Resume v join v.tags t where t in (:tags) group by (v.id)", Resume.class)
                 .setParameter("tags", tags);
-        return getResumes(query, tags, limit, page);
-    }
-
-    private Page<Resume> getResumes(Query query, Set<Tag> tags, int limit, int page) {
         long totalElements = (long) entityManager
                 .createQuery("select count(distinct v) from Resume v join v.tags t where t in (:tags)")
                 .setParameter("tags", tags)
                 .getSingleResult();
 
-        int totalPages = (int) (Math.ceil((double) totalElements / (double) limit));
+        int totalPages = getTotalPages(totalElements, limit);
+        List<Resume> resumeList = query.setFirstResult(page * limit).setMaxResults(limit).getResultList();
 
-        List ids = query.setFirstResult(page * limit).setMaxResults(limit).getResultList();
-        List<Long> resumeIds = new ArrayList<>();
-        for (Object resume : ids) {
-            resumeIds.add((Long) ((Object[]) resume)[0]);
-        }
+        return new ResumePageDTO(resumeList, totalPages);
+    }
 
-        List<Resume> resumeList = entityManager.createQuery("select v from Resume v where v.id in (:ids)")
-                .setParameter("ids", resumeIds)
-                .getResultList();
-
-        List<Resume> resumes = new ArrayList<>();
-        for (Long id : resumeIds) {
-            for (Resume r : resumeList) {
-                if (r.getId().equals(id)) {
-                    resumes.add(r);
-                }
-            }
-        }
-        return new ResumePageDTO(resumes, totalPages);
+    private int getTotalPages(long totalElements, int limit) {
+        return (int) (Math.ceil((double) totalElements / (double) limit));
     }
 }
