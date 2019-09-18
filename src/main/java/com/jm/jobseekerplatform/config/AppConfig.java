ded3dfd27a9b9baaeb224778217c84710e880bb4
AghanimTheDeveloper <aghaniman@gmail.com>
2019-09-18 12:59:58 +0300

    stable invitation commit

--- src/main/java/com/jm/jobseekerplatform/config/AppConfig.java
+++ src/main/java/com/jm/jobseekerplatform/config/AppConfig.java
@@ -1,14 +1,7 @@
 package com.jm.jobseekerplatform.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
-
-import java.util.Properties;
 
 @Configuration
 @PropertySource({
