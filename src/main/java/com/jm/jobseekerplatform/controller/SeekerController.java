46c0258f1ea2fd3885d3b8569489bcbb2f45e6b1
fumly <mozartiney@yahoo.com>
2019-09-24 17:49:38 +0300

    Update SeekerController.java

--- src/main/java/com/jm/jobseekerplatform/controller/SeekerController.java
+++ src/main/java/com/jm/jobseekerplatform/controller/SeekerController.java
@@ -20,7 +20,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import javax.annotation.security.RolesAllowed;
 import java.util.List;
 import java.util.Set;
