0cef9b392c1e1dd1fc4e85575a6f050cd03fca38
xitrbly <xitrbly@yandex.ru>
2019-09-27 04:21:55 +0300

    Updated login failure handler

--- src/main/java/com/jm/jobseekerplatform/config/LoginFailureHandler.java
+++ src/main/java/com/jm/jobseekerplatform/config/LoginFailureHandler.java
@@ -14,18 +14,20 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class LoginFailureHandler extends SimpleUrlAuthenticationFailureHandler {
 
     @Autowired
     private UserService userService;
 
-    LoginFailureHandler(UserService userService) {
+    public LoginFailureHandler(UserService userService) {
         this.userService = userService;
     }
 
     @Override
-    public void onAuthenticationFailure(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException exception) throws IOException, ServletException {
+    public void onAuthenticationFailure(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+                                        AuthenticationException exception) throws IOException, ServletException {
         httpServletRequest.getSession().invalidate();
         if (exception.getMessage().equals("User is disabled")) {
             User user = userService.findByEmail(httpServletRequest.getParameter("j_username"));
@@ -33,28 +35,29 @@ public class LoginFailureHandler extends SimpleUrlAuthenticationFailureHandler {
             LocalDateTime date = expireDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
             LocalDateTime localDate = LocalDateTime.now();
             long diff = ChronoUnit.SECONDS.between(localDate, date);
-            long howManyDays = diff/60/60/24;
-            long howManyHours = (diff - howManyDays)/60/60;
-            long howManyMinutes = (diff - (howManyDays*60*60*24) - (howManyHours*60*60))/60;
-            long howManySeconds = (diff - (howManyDays*60*60*24) - (howManyHours*60*60) - (howManyMinutes*60));
-            StringBuilder timeRemaining = new StringBuilder("До конца блокировки осталось: ");
+            long howManyDays = TimeUnit.SECONDS.toDays(diff);
+            long howManyHours = TimeUnit.SECONDS.toHours(diff - TimeUnit.DAYS.toSeconds(howManyDays));
+            long howManyMinutes = TimeUnit.SECONDS.toMinutes(diff - TimeUnit.DAYS.toSeconds(howManyDays)
+                    - TimeUnit.HOURS.toSeconds(howManyHours));
+            long howManySeconds = (diff - TimeUnit.DAYS.toSeconds(howManyDays) - TimeUnit.HOURS.toSeconds(howManyHours)
+                    - TimeUnit.MINUTES.toSeconds(howManyMinutes));
+            StringBuilder timeRemaining = new StringBuilder("Ваша учетная запись заблокирована. " +
+                    "До конца блокировки осталось: ");
             timeRemaining.append(howManyDays == 0 ? "" : howManyDays +  " дней, ")
                          .append(howManyHours == 0 ? "" : howManyHours + " часов, ")
                          .append(howManyMinutes == 0 ? "" : howManyMinutes + " минут, ")
                          .append(howManySeconds == 0 ? "" : howManySeconds + " секунд.");
-            httpServletRequest.getSession().setAttribute("disabled", timeRemaining);
+            httpServletRequest.getSession().setAttribute("error", timeRemaining);
             httpServletResponse.sendRedirect("/login?timeRemaining");
         } else if (exception.getMessage().equalsIgnoreCase("Bad credentials")) {
             httpServletRequest.getSession().setAttribute("error", "Неверный логин или пароль.");
             httpServletResponse.sendRedirect("/login?error");
-        }
-        else if (exception.getMessage().equalsIgnoreCase("User account has expired")) {
-            httpServletRequest.getSession().setAttribute("expired",
+        } else if (exception.getMessage().equalsIgnoreCase("User account has expired")) {
+            httpServletRequest.getSession().setAttribute("error",
                     "Срок действия учетной записи истек.");
             httpServletResponse.sendRedirect("/login?expired");
-        }
-        else if (exception.getMessage().equalsIgnoreCase("blocked")) {
-            httpServletRequest.getSession().setAttribute("blocked", "Ваша учетная запись заблокирована.");
+        } else if (exception.getMessage().equalsIgnoreCase("blocked")) {
+            httpServletRequest.getSession().setAttribute("error", "Ваша учетная запись заблокирована.");
             httpServletResponse.sendRedirect("/login?blocked");
         } else {
             httpServletResponse.sendRedirect("/login?unknownError");
