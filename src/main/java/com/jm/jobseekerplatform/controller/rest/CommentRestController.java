1a3be1d11294d06e2f24ff70fc9b1e821daa1164
Vermodje <mr.ryabchikov@list.ru>
2019-10-01 16:54:28 +0300

    01.10.2019

--- src/main/java/com/jm/jobseekerplatform/controller/rest/CommentRestController.java
+++ src/main/java/com/jm/jobseekerplatform/controller/rest/CommentRestController.java
@@ -2,6 +2,7 @@ package com.jm.jobseekerplatform.controller.rest;
 
 import com.jm.jobseekerplatform.model.News;
 import com.jm.jobseekerplatform.model.comments.Comment;
+import com.jm.jobseekerplatform.model.profiles.Profile;
 import com.jm.jobseekerplatform.model.users.User;
 import com.jm.jobseekerplatform.service.impl.NewsService;
 import com.jm.jobseekerplatform.service.impl.comments.CommentService;
@@ -13,36 +14,34 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
-
 @RestController
-@RequestMapping(value="/api/comments")
+@RequestMapping (value = "/api/comments")
 public class CommentRestController {
     @Autowired
     private CommentService commentService;
     @Autowired
     private NewsService newsService;
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @RequestMapping (value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity<List<Comment>> getAllCommentsForNews(@PathVariable Long id) {
         List<Comment> commentList = new ArrayList<>();
         News news = newsService.getById(id);
           try{
               commentList = commentService.getAllCommentsForNews(news);
-
           } catch (Exception e) {
               e.printStackTrace();
           }
         return new ResponseEntity<>(commentList, HttpStatus.OK);
     }
 
-    @RequestMapping(params = {"id"}, method = RequestMethod.GET)
+    @RequestMapping (params = {"id"}, method = RequestMethod.GET)
     public ResponseEntity<Comment> getComment(@RequestParam("id") Long id) {
         Comment comment = commentService.getById(id);
         return new ResponseEntity<Comment>(comment, HttpStatus.OK);
     }
 
-   @RequestMapping(value = "/update", params = {"id", "text", "dateTime"},
-           method = RequestMethod.PUT)
+   @RequestMapping (value = "/update", params = {"id", "text", "dateTime"},
+                                               method = RequestMethod.PUT)
     public  ResponseEntity<Comment> updateComment(@RequestParam("id") Long id,
                                                   @RequestParam("text") String text,
                                                   @RequestParam("dateTime") String dateTime) {
@@ -53,20 +52,21 @@ public class CommentRestController {
         return new ResponseEntity<Comment>(comment, HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/delete", params={"id"}, method = RequestMethod.DELETE)
+    @RequestMapping (value = "/delete", params = {"id"}, method = RequestMethod.DELETE)
     public ResponseEntity<Comment> deleteComment(@RequestParam("id") Long id) {
        commentService.deleteById(id);
        return new ResponseEntity<Comment>(HttpStatus.NO_CONTENT);
     }
 
     @RequestMapping(value = "/insert", params = {"newsId", "text", "dateTime"},
-            method = RequestMethod.POST)
+                                                  method = RequestMethod.POST)
     public ResponseEntity<Void> createComment(@RequestParam("newsId") Long id,
                                               @RequestParam("text") String text,
                                               @RequestParam("dateTime") String dateTime,
                                               Authentication authentication) {
-        Comment comment = new Comment(text,newsService.getById(id),
-                ((User) authentication.getPrincipal()).getProfile(), dateTime);
+        News news = newsService.getById(id);
+        Profile profile =((User) authentication.getPrincipal()).getProfile();
+        Comment comment = new Comment(text, news, profile, dateTime);
         commentService.add(comment);
         return new ResponseEntity<Void>(HttpStatus.CREATED);
     }
