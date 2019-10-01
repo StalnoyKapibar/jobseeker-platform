125e6bbaac085f8286078eacc003714ea9eea12b
Vermodje <mr.ryabchikov@list.ru>
2019-10-01 17:05:01 +0300

    01.10.2019

--- src/main/java/com/jm/jobseekerplatform/model/Tag.java
+++ src/main/java/com/jm/jobseekerplatform/model/Tag.java
@@ -1,7 +1,7 @@
 package com.jm.jobseekerplatform.model;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.jm.jobseekerplatform.model.comments.Comment;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.jm.jobseekerplatform.model.profiles.SeekerProfile;
 
 import javax.persistence.*;
 import java.io.Serializable;
