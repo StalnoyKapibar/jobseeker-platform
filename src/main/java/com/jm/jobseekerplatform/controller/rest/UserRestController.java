98ec2afd21c70e213bf5fdf6a1dc4b6937c4bd1d
ASD <asd555@gmail.com>
2019-10-26 22:26:58 +0300

    all ok without solved conflicts

--- src/main/java/com/jm/jobseekerplatform/controller/rest/UserRestController.java
+++ src/main/java/com/jm/jobseekerplatform/controller/rest/UserRestController.java
@@ -7,7 +7,9 @@ import javax.annotation.security.RolesAllowed;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.session.SessionInformation;
 import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,112 +29,114 @@ import com.jm.jobseekerplatform.service.impl.users.UserService;
 @RequestMapping("/api/users")
 public class UserRestController {
 
-    @Autowired
-    private UserService userService;
+	@Autowired
+	private UserService userService;
 
-    @Autowired
-    private MailService mailService;
+	@Autowired
+	private MailService mailService;
 
-    @Autowired
-    private ProfileService profileService;
+	@Autowired
+	private ProfileService profileService;
 
-    @Autowired
-    private SessionRegistry sessionRegistry;
+	@Autowired
+	private SessionRegistry sessionRegistry;
 
-    @GetMapping
-    public List<User> getAllUsers() {
-        return userService.getAll();
-    }
+	@GetMapping
+	public List<User> getAllUsers() {
+		return userService.getAll();
+	}
 
-    @RolesAllowed({"ROLE_ADMIN"})
-    @GetMapping("/block/{id}/{periodInDays}")
-    public ResponseEntity blockUser(@PathVariable("id") Long id,
-                                    @PathVariable Integer periodInDays) {
-        User user = userService.getById(id);
-        user.setEnabled(false);
-        userService.update(user);
+	@RolesAllowed({ "ROLE_ADMIN" })
+	@GetMapping("/block/{id}/{periodInDays}")
+	public ResponseEntity blockUser(@PathVariable("id") Long id, @PathVariable Integer periodInDays) {
+		User user = userService.getById(id);
+		user.setEnabled(false);
+		userService.update(user);
 
-        List<Object> allPrincipals = sessionRegistry.getAllPrincipals();
-        if (!allPrincipals.isEmpty()) {
-            allPrincipals.forEach(o -> {
-                User u = (User) o;
-                if(u.getId().equals(user.getId())) {
-                    List<SessionInformation> allSessions = sessionRegistry.getAllSessions(u, false);
-                    allSessions.forEach(SessionInformation::expireNow);
-                }
-            });
-        }
+		List<Object> allPrincipals = sessionRegistry.getAllPrincipals();
+		if (!allPrincipals.isEmpty()) {
+			allPrincipals.forEach(o -> {
+				User u = (User) o;
+				if (u.getId().equals(user.getId())) {
+					List<SessionInformation> allSessions = sessionRegistry.getAllSessions(u, false);
+					allSessions.forEach(SessionInformation::expireNow);
+				}
+			});
+		}
 
-        Profile profile = user.getProfile();
-        if (periodInDays == 0) {
-            profileService.blockPermanently(profile);
-        }
-        if (periodInDays > 0 && periodInDays < 15) {
-            profileService.blockTemporary(profile, periodInDays);
-        }
+		Profile profile = user.getProfile();
+		if (periodInDays == 0) {
+			profileService.blockPermanently(profile);
+		}
+		if (periodInDays > 0 && periodInDays < 15) {
+			profileService.blockTemporary(profile, periodInDays);
+		}
 
-        return new ResponseEntity(HttpStatus.OK);
-    }
+		return new ResponseEntity(HttpStatus.OK);
+	}
 
-    @RolesAllowed({"ROLE_ADMIN"})
-    @GetMapping("/unblock/{id}")
-    public ResponseEntity unblockUser(@PathVariable("id") Long id) {
-        User user = userService.getById(id);
-        user.setEnabled(true);
-        userService.update(user);
+	@RolesAllowed({ "ROLE_ADMIN" })
+	@GetMapping("/unblock/{id}")
+	public ResponseEntity unblockUser(@PathVariable("id") Long id) {
+		User user = userService.getById(id);
+		user.setEnabled(true);
+		userService.update(user);
 
-        Profile profile = user.getProfile();
-        profileService.unblock(profile);
+		Profile profile = user.getProfile();
+		profileService.unblock(profile);
 
-        return new ResponseEntity(HttpStatus.OK);
-    }
+		return new ResponseEntity(HttpStatus.OK);
+	}
 
-    @RequestMapping(method = RequestMethod.POST, value = "/add")
-    public void registerNewUser(@RequestBody User user) {
-        if (userService.validateNewUser(user)) {
-            userService.registerNewUser(user);
-        }
-    }
+	@RequestMapping(method = RequestMethod.POST, value = "/add")
+	public void registerNewUser(@RequestBody User user) {
+		if (userService.validateNewUser(user)) {
+			userService.registerNewUser(user);
+		}
+	}
 
-    @RequestMapping(method = RequestMethod.POST, value = "/addUserByAdmin/{check}")
-    public void addNewUser(@RequestBody User user, @PathVariable boolean check) {
-        if (userService.validateNewUser(user)) {
-            userService.addNewUserByAdmin(user, check);
-        }
-    }
+	@RequestMapping(method = RequestMethod.POST, value = "/addUserByAdmin/{check}")
+	public void addNewUser(@RequestBody User user, @PathVariable boolean check) {
+		if (userService.validateNewUser(user)) {
+			userService.addNewUserByAdmin(user, check);
+		}
+	}
 
-    //     проверка валидации user через ajax
-    @RequestMapping(method = RequestMethod.GET, value = "/email/{email}")
-    public ResponseEntity<Boolean> isExistEmail(@PathVariable String email) {
-        return ResponseEntity.ok(userService.isExistEmail(email));
-    }
+	// проверка валидации user через ajax
+	@RequestMapping(method = RequestMethod.GET, value = "/email/{email}")
+	public ResponseEntity<Boolean> isExistEmail(@PathVariable String email) {
+		return ResponseEntity.ok(userService.isExistEmail(email));
+	}
 
-    @RequestMapping(method = RequestMethod.GET, value = "/getUser/{userId}")
-    public User getUser(@PathVariable(required = false) Long userId) {
-        return userService.getById(userId);
-    }
+	@RequestMapping(method = RequestMethod.GET, value = "/getUser/{userId}")
+	public User getUser(@PathVariable(required = false) Long userId) {
+		return userService.getById(userId);
+	}
 
-    // отправка приглашения
-    @RequestMapping(method = RequestMethod.GET, value = "/inviteFriend/{user}/{friend}")
-    public void inviteFriend(@PathVariable String user, @PathVariable String friend) {
-        mailService.sendFriendInvitaionEmail(user, friend);
-    }
+	// отправка приглашения
+	@RequestMapping(method = RequestMethod.GET, value = "/inviteFriend/{user}/{friend}")
+	public void inviteFriend(@PathVariable String user, @PathVariable String friend) {
+		mailService.sendFriendInvitaionEmail(user, friend);
+	}
 
-    //запрос на востановление пароля
-    @RequestMapping(method = RequestMethod.GET, value = "/recovery/{email}")
-    public ResponseEntity<Boolean> recoveryPassRequest(@PathVariable String email) {
-        return ResponseEntity.ok(userService.recoveryPassRequest(email));
-    }
+	// запрос на востановление пароля
+	@RequestMapping(method = RequestMethod.GET, value = "/recovery/{email}")
+	public ResponseEntity<Boolean> recoveryPassRequest(@PathVariable String email) {
+		return ResponseEntity.ok(userService.recoveryPassRequest(email));
+	}
 
-    //востановление пароля
-    @RequestMapping(method = RequestMethod.GET, value = "/password_reset/{token}/{password}")
-    public void newPassword(@PathVariable String token, @PathVariable char[] password) {
-        userService.passwordReset(token, password);
-    }
-    
-    @GetMapping("/getuserrole")
-    public String getUserRole(Authentication authentication){
-    	User currUser=(User) authentication.getPrincipal();
-    	return currUser.getProfile().getTypeName();
-    }
+	// востановление пароля
+	@RequestMapping(method = RequestMethod.GET, value = "/password_reset/{token}/{password}")
+	public void newPassword(@PathVariable String token, @PathVariable char[] password) {
+		userService.passwordReset(token, password);
+	}
+
+	@GetMapping("/get-user-authority")
+	public GrantedAuthority getUserAuthority(Authentication authentication) {
+		if (!(authentication instanceof AnonymousAuthenticationToken)) {
+		User currUser = (User) authentication.getPrincipal();
+		return currUser.getAuthority();
+		}
+		return null;
+	}
 }
